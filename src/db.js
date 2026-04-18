3c629bb62321f697e322412db2e63ed03c895701
Teja <teja14950@gmail.com>
2026-04-19 02:00:14 +0530

    fix: security

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@ const {Pool} = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: "6281@Tma",
-  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
-
+console.log(process.env.DB_USER);
 module.exports = pool;
