9d32c44fdfd4d9dec90b7bfe162ac64c4c1edc58
Teja <teja14950@gmail.com>
2026-05-11 01:16:41 +0530

    Making in frontend and backend in one repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import RoomMap from "./components/RoomMap";
+
+function App() {
+  return (
+    <div>
+      <h1>Campus Asset System</h1>
+      <RoomMap />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/RoomMap.jsx
@@ -0,0 +1,9 @@
+function RoomMap() {
+  return (
+    <div>
+      <h2>Room Map</h2>
+    </div>
+  );
+}
+
+export default RoomMap;
